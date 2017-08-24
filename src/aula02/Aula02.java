7b6bdcd648f07957845d74622eac3d43dca46b66
aluno <andredomingues.556@gmail.com>
2017-08-23 21:49:53 -0400

    Aula de variaveis java

---
+++ src/aula02/Aula02.java
@@ -0,0 +1,21 @@
+package aula02;
+public class Aula02 {
+
+    public static void main(String[] args) {
+    int x = 10;
+    //variavel tipo int
+      System.out.println("Variavel int:"+x);
+    float y = (float) 2.5;
+    // variavel float
+        System.out.println("Variavel float:"+y);
+    
+    
+
+    
+    
+           
+    }
+   
+    
+    
+}
